373acc4061c666cc2187341831e661d52315805c
Matman777 <103330663+Matman777@users.noreply.github.com>
2022-06-05 13:26:43 +0200

    Update liste_de_courses_originale.py

--- liste_de_courses_originale.py
+++ liste_de_courses_originale.py
@@ -72,6 +72,6 @@ while True:
             print(f"{i}. {element.capitalize()}")
             
         
-     elif user_choice == "4":
+    elif user_choice == "4":
         final_lst.clear()
         print("La liste a été vidée de son contenu.")
